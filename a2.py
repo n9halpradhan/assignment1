a54c19ba5393b5f79c1456f48b4b6c0128281817
Nihal Pradhan <55305935+n9halpradhan@users.noreply.github.com>
2021-02-10 10:24:33 +0530

    Update a2.py

--- a2.py
+++ a2.py
@@ -1,16 +1,7 @@
 print("Enter marks of two subjects: ")
-S1=float(input())
-S2=float(input())
-S3=float(input())
-S4=float(input())
-S5=float(input())
-avg=(S1+S2+S3+S4+S5)/5;
-print("Average marks = ", avg)
-if avg>=90:
-    print("Grade O")
-elif avg>=80:
-    print("Grade E")
-elif avg>=70:
-    print("Grade A")
-else:
-    print("Grade B")
+S1=int(input())
+S2=int(input())
+Total = S1 + S2;
+Average = Total/2;
+print("Total marks = ", Total)
+print("Average marks = ", Average)
